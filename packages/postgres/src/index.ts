9f897fbeb4a5a58ad8e88127f34a90adcfa071e8
mrabaev48 <mrabaev48@gmail.com>
2025-09-17 02:16:13 +0300

    build(providers): minimize provider exports (drop core re-exports)

--- packages/postgres/src/index.ts
+++ packages/postgres/src/index.ts
@@ -2,9 +2,6 @@
  * PostgreSQL database provider for ts-linq ORM
  */
 
-// Re-export core types for convenience
-export * from '@ts-linq/core';
-
 // PostgreSQL-specific exports
 export * from './PostgresProvider';
 export * from './PostgresDialect';
