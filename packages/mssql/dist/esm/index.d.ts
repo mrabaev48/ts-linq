75f25fea49d5c460cb4fff84ad3839257b3ff532
mrabaev48 <mrabaev48@gmail.com>
2025-09-17 15:29:39 +0300

    build: update build outputs and configs (tree-shaking, size CI, tsc -b)

--- packages/mssql/dist/esm/index.d.ts
+++ packages/mssql/dist/esm/index.d.ts
@@ -1,7 +1,6 @@
 /**
  * Microsoft SQL Server database provider for ts-linq ORM
  */
-export * from '@ts-linq/core';
 export * from './MssqlProvider';
 export * from './MssqlDialect';
 export * from './MssqlDdlStrategy';
--- packages/mysql/dist/index.js
+++ packages/mysql/dist/index.js
@@ -17,8 +17,6 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// Re-export core types for convenience
-__exportStar(require("@ts-linq/core"), exports);
 // MySQL-specific exports
 __exportStar(require("./MySqlProvider"), exports);
 __exportStar(require("./MysqlDialect"), exports);
