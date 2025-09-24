0bc9d964b17068b46fbfa283cba9e5fa1eede1dd
mikhail.soosaar <mikhail.soosaar@xdatagroup.io>
2025-09-25 02:45:02 +0300

    feat(pkg:open-telemetry-sql-logger): extract OTEL SqlLogger to separate package\nchore(core): remove heavy utils exports; stub OpenTelemetrySqlLogger; update tests\nchore(workspaces): add open-telemetry-sql-logger workspace

--- packages/core/dist/utils/OpenTelemetrySqlLogger.js
+++ packages/core/dist/utils/OpenTelemetrySqlLogger.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.OpenTelemetrySqlLogger = void 0;
 function safeRequireOtel() {
     try {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -17,45 +16,5 @@ function safeRequireOtel() {
  * OpenTelemetry-based SqlLogger implementation.
  * Uses dynamic require to avoid hard dependency on @opentelemetry/api.
  */
-class OpenTelemetrySqlLogger {
-    constructor(serviceName = 'ts-linq') {
-        this.spanByTraceId = new Map();
-        const otel = safeRequireOtel();
-        this.tracer = otel?.trace.getTracer(serviceName);
-    }
-    queryStart(info) {
-        if (!this.tracer)
-            return;
-        const span = this.tracer.startSpan('db.query', {
-            attributes: {
-                'db.system': info.provider || 'sql',
-                'db.statement': info.sql,
-                'db.parameters': JSON.stringify(info.params ?? [])
-            }
-        });
-        this.spanByTraceId.set(info.traceId, span);
-    }
-    queryEnd(info) {
-        const span = this.spanByTraceId.get(info.traceId);
-        if (!span)
-            return;
-        try {
-            span.setAttribute('db.duration_ms', info.durationMs);
-            if (typeof info.rows === 'number')
-                span.setAttribute('db.rows', info.rows);
-            if (info.error) {
-                span.recordException({ name: info.error.name, message: info.error.message });
-                span.setStatus({ code: 2, message: info.error.message }); // 2 = ERROR
-            }
-            else {
-                span.setStatus({ code: 1 }); // 1 = OK
-            }
-        }
-        finally {
-            span.end();
-            this.spanByTraceId.delete(info.traceId);
-        }
-    }
-}
-exports.OpenTelemetrySqlLogger = OpenTelemetrySqlLogger;
+// Moved to package 'open-telemetry-sql-logger'
 //# sourceMappingURL=OpenTelemetrySqlLogger.js.map
